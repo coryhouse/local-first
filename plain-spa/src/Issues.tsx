61394d780509f7d74f90bd74ba7ff7641bac335d
Cory House <housecor@gmail.com>
2025-06-22 17:35:40 -0500

    add heading

--- plain-spa/src/Issues.tsx
+++ plain-spa/src/Issues.tsx
@@ -1,18 +1,21 @@
 export function Issues() {
   return (
-    <details>
-      <summary>Plain SPA Issues</summary>
-      <ol>
-        <li>Slow connection or slow backend means slow UX</li>
-        <li>
-          Must implement loading state because of #1, or use optimistic UI which
-          is tedious and error prone
-        </li>
-        <li>No offline support</li>
-        <li>May be viewing stale data</li>
-        <li>No conflict handling. Last in quietly wins.</li>
-        <li>No cross-browser tab syncing</li>
-      </ol>
-    </details>
+    <>
+      <h2>What's wrong with this?</h2>
+      <details>
+        <summary>Plain SPA Issues</summary>
+        <ol>
+          <li>Slow connection or slow backend means slow UX</li>
+          <li>
+            Must implement loading state because of #1, or use optimistic UI
+            which is tedious and error prone
+          </li>
+          <li>No offline support</li>
+          <li>May be viewing stale data</li>
+          <li>No conflict handling. Last in quietly wins.</li>
+          <li>No cross-browser tab syncing</li>
+        </ol>
+      </details>
+    </>
   );
 }
