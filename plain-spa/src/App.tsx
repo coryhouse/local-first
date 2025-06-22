cc10f0f66c4beed03d8e7eb746d67784b31ed74c
Cory House <housecor@gmail.com>
2025-06-21 20:12:12 -0500

    a

--- plain-spa/src/App.tsx
+++ plain-spa/src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Vehicle } from "./types/Vehicle";
 
-const emptyVehicle = {
+const emptyVehicle: Vehicle = {
   id: "",
   make: "",
   model: "",
   year: null,
+  price: null,
+  sold: false,
 };
 
 export default function Inventory() {
@@ -35,27 +37,47 @@ export default function Inventory() {
     }));
   }
 
-  function onVehicleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function onVehicleInputChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+    vehicle: Vehicle
+  ) {
     const { value } = e.target;
-    const id = e.target.closest("li")?.getAttribute("data-id");
-    if (id) {
-      setVehicles((prev) =>
-        prev.map((v) =>
-          v.id === id
-            ? {
-                ...v,
-                [e.target.placeholder.toLowerCase()]:
-                  e.target.type === "number" ? Number(value) : value,
-              }
-            : v
-        )
-      );
+    setVehicles((prev) =>
+      prev.map((v) =>
+        v.id === vehicle.id
+          ? {
+              ...v,
+              [e.target.name]: value,
+            }
+          : v
+      )
+    );
+  }
+
+  function onVehicleInputBlur(
+    e: React.FocusEvent<HTMLInputElement>,
+    vehicle: Vehicle
+  ) {
+    const { value } = e.target;
+    if (e.target.name === "price" && value) {
+      const price = Number(value);
+      if (isNaN(price)) {
+        alert("Please enter a valid number for price.");
+        return;
+      }
+      fetch(`http://localhost:3001/vehicles/${vehicle.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ price }),
+      });
     }
   }
 
   return (
     <>
-      <h1>Inventory</h1>
+      <h1>Vehicles For Sale</h1>
 
       <h2>Add Vehicle</h2>
 
@@ -115,14 +137,31 @@ export default function Inventory() {
 
       {vehicles.map((v) => (
         <ul key={v.id}>
-          <li>
-            <input type="text" value={v.year} onChange={onVehicleInputChange} />{" "}
-            <input type="text" value={v.make} onChange={onVehicleInputChange} />{" "}
+          <li style={{ listStyleType: "none", paddingLeft: "0" }}>
+            {`${v.year} ${v.make} ${v.model} -`}${" "}
             <input
               type="text"
-              value={v.model}
-              onChange={onVehicleInputChange}
-            />{" "}
+              name="price"
+              placeholder="Price"
+              value={v.price || ""}
+              onChange={(e) => onVehicleInputChange(e, v)}
+              onBlur={(e) => onVehicleInputBlur(e, v)}
+            />
+            <input
+              type="checkbox"
+              name="sold"
+              checked={v.sold}
+              onChange={(e) => {
+                setVehicles((prev) =>
+                  prev.map((vehicle) =>
+                    vehicle.id === v.id
+                      ? { ...vehicle, sold: e.target.checked }
+                      : vehicle
+                  )
+                );
+              }}
+            />
+            <label>Sold</label>
             <button
               onClick={() => {
                 // TODO UPDATE
