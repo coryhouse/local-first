e8da64a73087bfec1110965afec6395f6eecea51
Cory House <housecor@gmail.com>
2025-06-23 14:49:47 -0500

    a

---
+++ convex-react/convex/schema.ts
@@ -0,0 +1,25 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+import { authTables } from "@convex-dev/auth/server";
+
+// The schema is normally optional, but Convex Auth
+// requires indexes defined on `authTables`.
+// The schema provides more precise TypeScript types.
+export default defineSchema({
+  ...authTables,
+  vehicles: defineTable({
+    _id: v.id("vehicles"),
+    make: v.string(),
+    model: v.string(),
+    year: v.number(),
+    price: v.number(),
+    status: v.union(
+      v.literal("on sale"),
+      v.literal("sold"),
+      v.literal("reconditioning"),
+    ),
+  }),
+  numbers: defineTable({
+    value: v.number(),
+  }),
+});
