84b24cdaaecbf7241c6b2c21abeaf211de5ac6ca
Cory House <housecor@gmail.com>
2025-06-24 17:49:06 -0500

    fix

--- convex-react/src/components/Select.tsx
+++ convex-react/src/components/Select.tsx
@@ -1,11 +1,11 @@
 export function Select({
   children,
-  onChange,
+  ...rest
 }: React.SelectHTMLAttributes<HTMLSelectElement>) {
   return (
     <select
       className="bg-light dark:bg-dark text-dark dark:text-light rounded-md p-2 border-2 border-slate-200 dark:border-slate-800 mr-2"
-      onChange={onChange}
+      {...rest}
     >
       {children}
     </select>
