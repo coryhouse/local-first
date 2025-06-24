32524e461ff87d9709feaf4f1de520da826a3815
Cory House <housecor@gmail.com>
2025-06-24 06:36:50 -0500

    style fix

--- convex-react/src/components/Input.tsx
+++ convex-react/src/components/Input.tsx
@@ -8,6 +8,7 @@ export function Input({
   return (
     <input
       type={type}
+      className="bg-light dark:bg-dark text-dark dark:text-light rounded-md p-2 border-2 border-slate-200 dark:border-slate-800 mr-2"
       placeholder={placeholder}
       value={value}
       onChange={onChange}
