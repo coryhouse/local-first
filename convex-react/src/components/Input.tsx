2bd010744da7960d87d1ef1cda8a2490e81a7aa8
Cory House <housecor@gmail.com>
2025-06-24 06:38:43 -0500

    accept style

--- convex-react/src/components/Input.tsx
+++ convex-react/src/components/Input.tsx
@@ -4,15 +4,18 @@ export function Input({
   value,
   onChange,
   onBlur,
+  className,
+  ...rest
 }: React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       type={type}
-      className="bg-light dark:bg-dark text-dark dark:text-light rounded-md p-2 border-2 border-slate-200 dark:border-slate-800 mr-2"
+      className={`bg-light dark:bg-dark text-dark dark:text-light rounded-md p-2 border-2 border-slate-200 dark:border-slate-800 mr-2${className ? ` ${className}` : ""}`}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
       onBlur={onBlur}
+      {...rest}
     />
   );
 }
