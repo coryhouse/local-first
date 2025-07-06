34b0918ce75bcdb4da7d5df088dbd83852aa17f3
Cory House <housecor@gmail.com>
2025-07-06 11:44:41 -0500

    a

--- electric-sql-with-react/src/main.tsx
+++ electric-sql-with-react/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ErrorBoundary } from "react-error-boundary";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster position="top-right" richColors />
     <ErrorBoundary fallback={<div>Something went wrong</div>}>
       <App />
     </ErrorBoundary>
