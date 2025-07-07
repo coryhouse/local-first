ea26934ca4cb0f56352c04a9dc9d9ee130eeeb02
Cory House <housecor@gmail.com>
2025-07-07 14:19:29 -0500

    fix

--- plain-spa/vite.config.ts
+++ plain-spa/vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    hmr: false, // Disabled because with this true, the app hard reloads after each HTTP call. Only happens when Tailwind is used. Can comment out index.css to resolve.
+  },
 });
