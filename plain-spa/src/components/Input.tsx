1ce0d3dafff0c5a111dbaaf48f85b8f8dc9428c5
Cory House <housecor@gmail.com>
2025-07-06 20:21:54 -0500

    simplify

--- plain-spa/src/components/Input.tsx
+++ plain-spa/src/components/Input.tsx
@@ -1,20 +1,12 @@
 export function Input({
-  type,
-  placeholder,
-  value,
-  onChange,
-  onBlur,
   className,
   ...rest
 }: React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
-      type={type}
-      className={`bg-light dark:bg-dark text-dark dark:text-light rounded-md p-2 border-2 border-slate-200 dark:border-slate-800 mr-2${className ? ` ${className}` : ""}`}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      onBlur={onBlur}
+      className={`bg-light dark:bg-dark text-dark dark:text-light rounded-md p-2 border-2 border-slate-200 dark:border-slate-800 mr-2${
+        className ? ` ${className}` : ""
+      }`}
       {...rest}
     />
   );
