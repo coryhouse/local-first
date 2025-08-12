87743a42c0dc19c1503103be555affcd8b853212
Cory House <housecor@gmail.com>
2025-08-12 11:54:31 -0500

    a

--- plain-spa/src/main.tsx
+++ plain-spa/src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "sonner";
+import { ErrorBoundary } from "react-error-boundary";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Toaster position="top-right" richColors />
-    <App />
+    <ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
